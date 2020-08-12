716609d625e0a459c2db261d495f56ce30dcba15
HP <efafafaef>
2020-08-12 23:35:38 +0300

    feat: add 'escape' button

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 let registerButton = document.querySelector('.register-button');
 let loginButton = document.querySelector('.login-button');
-let escapeButton = document.querySelector('.escape');
+let escapeButton = document.querySelectorAll('.escape');
 
 let wrapper = document.querySelector('.wrapper');
 let entryWindow = document.querySelector('.entry-window');
+let loginWindow = document.querySelector('.login-window');
+let modalWindow = document.querySelectorAll('.modal');
 let fogging = document.querySelector('.fogging');
 
-// Модальное окно для регистрации
-registerButton.addEventListener('click', function() {
-	entryWindow.style.display = 'block';
+// Затемнение и блокировка при появлении модального окна
+function blockAndFogging() {
 	document.body.classList.add('body-lock');
 	fogging.style.display = 'block';
+}
+
+// Вызов модального окна для регистрации
+registerButton.addEventListener('click', function() {
+	blockAndFogging();
+	entryWindow.style.display = 'block';
 });
 
-// Модальное окно для входа
+// Вызов модального окна для входа
 loginButton.addEventListener('click', function() {
-	let login = document.querySelector('.login-window');
-	login.style.display = 'flex';
+	blockAndFogging();
+	loginWindow.style.display = 'flex';
 });
 
 // Закрытие модального окна
-escapeButton.addEventListener('click', function() {
-	let window = document.querySelector('.modal');
-	window.style.display = 'none';
-	fogging.style.display = 'none';
-	document.querySelector('form').reset();
+escapeButton.forEach(item => {
+	item.addEventListener('click', function() {
+		item.closest('.modal').style.display = 'none';
+		fogging.style.display = 'none';
+		let parent = item.parentElement;
+		let form = parent.children[1];
+		form.reset();
+	});
 });
