77cd28d36a96a3e5598d29709116e0c8f8ea58b0
HP <efafafaef>
2020-08-13 21:15:34 +0300

    feat: add 'confirm registration' button

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let registerButton = document.querySelector('.register-button');
 let loginButton = document.querySelector('.login-button');
 let escapeButton = document.querySelectorAll('.escape');
+let confirmRegistrationButton = document.querySelector('.confirm-registration');
 
 let wrapper = document.querySelector('.wrapper');
-let entryWindow = document.querySelector('.entry-window');
+let registerWindow = document.querySelector('.register-window');
 let loginWindow = document.querySelector('.login-window');
 let modalWindow = document.querySelectorAll('.modal');
 let fogging = document.querySelector('.fogging');
@@ -17,7 +18,7 @@ function blockAndFogging() {
 // Вызов модального окна для регистрации
 registerButton.addEventListener('click', function() {
 	blockAndFogging();
-	entryWindow.style.display = 'block';
+	registerWindow.style.display = 'block';
 });
 
 // Вызов модального окна для входа
@@ -36,3 +37,44 @@ escapeButton.forEach(item => {
 		form.reset();
 	});
 });
+
+// Отправка формы регистрации
+confirmRegistrationButton.addEventListener('click', function() {
+	let currentForm = document.forms.registerForm;
+	let login = currentForm.elements.login.value;
+	let password = currentForm.elements.password.value;
+	let name = currentForm.elements.username.value;
+	let age = currentForm.elements.age.value;
+	let newUser = new User(login, password, name, age); /*Создание нового пользователя на основании введённых данных*/
+
+	let check = true; /*Проверка на отсутствие пустых полей*/ 
+	while(check) {
+		for(let key in newUser) {
+			if(newUser[key] === '') {
+				alert('Необходимо заполнить все поля');
+				return check;
+			} 
+		}
+		check = false;
+	}
+	
+	userList.push(newUser); /*Добавление пользователя в пользовательский массив, закрытие окна*/
+	currentForm.reset();
+	registerWindow.style.display = 'none';
+	fogging.style.display = 'none';
+	console.log(userList);
+});
+
+//---------------------------------------------------------------------------------
+let userList = [];
+
+function User(login, password, name, age) {
+	this.login = login;
+	this.password = password;
+	this.name = name;
+	this.age = age;
+}
+
+
+
+
